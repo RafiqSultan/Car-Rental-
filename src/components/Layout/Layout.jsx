5e44c7a4718607dc6ad9e1508a9aa4549446b8a6
RafiqSultan <Rafiqaasultan@gmail.com>
2022-10-22 14:56:55 -0700

    CREATE header section

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+
+
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+// import Routers from "../../routers/Routers";
+
+const Layout = () => {
+  return (
+    <Fragment>
+     <Header />
+    </Fragment>
+  );
+};
+
+export default Layout;

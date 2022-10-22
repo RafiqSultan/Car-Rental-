74cf155af26751609850b60d65da90b5bbd5a16f
RafiqSultan <Rafiqaasultan@gmail.com>
2022-10-22 15:46:08 -0700

    CREATE home page

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,12 +3,15 @@ import React, { Fragment } from "react";
 
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
-// import Routers from "../../routers/Routers";
+import Routers from "../../routers/Routers";
 
 const Layout = () => {
   return (
     <Fragment>
      <Header />
+     <div>
+     <Routers />
+     </div>
      <Footer />
     </Fragment>
   );
